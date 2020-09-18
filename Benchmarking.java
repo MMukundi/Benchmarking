82615a327eb08f1a593d135c8560323ad0ffaef1
MMukundi <musicman5978@gmail.com>
2020-09-18 00:06:32 -0700

    Fixed bugs

--- Benchmarking.java
+++ Benchmarking.java
@@ -5,33 +5,152 @@ import java.util.Scanner;
 //https://en.wikipedia.org/wiki/ANSI_escape_code
 //ANSI escape codes were used here for debugging and printing progress
 
-class Benchmarking{
-    public static void main(String[] args) {
-        Benchmarker benchmarker= new Benchmarker();
-        for(String fileName:args){
-            try{
-            Scanner fileScanner = new Scanner(new File(fileName));
-            LinkedList<Integer> list = new LinkedList<Integer>();
+class Benchmarking {
+    public static void main(String[] args) throws Throwable {
+        if(args.length==0)args = getFiles();
+        for (String fileName : args) {
+            Benchmarker benchmarker = new Benchmarker();
+            try {
+                Scanner fileScanner = new Scanner(new File(fileName));
+                LinkedList<Integer> list = new LinkedList<Integer>();
+                LinkedList<Integer>.LinkedListIterator iter;
 
-            String benchmarkID = fileName+"_insert";
-            benchmarker.start(benchmarkID);
-            System.out.printf("Inserting the values from %s\n\033[s",fileName);
-            while(fileScanner.hasNextInt()){
-                int toInsert = fileScanner.nextInt();
-                int insertionIndex=0;
-                for(int otherValue: list){
-                    if(otherValue<toInsert)break;
-                    insertionIndex++;
+                // Using the built in iterator and the insert method result in slow insert times
+                // with this soltion
+                String benchmarkID = "insertNaive";
+                benchmarker.start(benchmarkID);
+                while (fileScanner.hasNextInt()) {
+                    int toInsert = fileScanner.nextInt();
+                    int insertionIndex = 0;
+                    for (int otherValue : list) {
+                        if (toInsert < otherValue)
+                            break;
+                        insertionIndex++;
+                    }
+                    list.insert(toInsert, insertionIndex);
+                    if(list.length%50000==0){
+                        System.out.printf("%d items\n",list.length);
+                    }
                 }
-                list.insert(toInsert,insertionIndex);
-                System.out.printf("\033[u\033[s%d values inserted",list.length);
-            } 
-            benchmarker.end(benchmarkID);
-            System.out.printf("\n%s took %d ns\n",benchmarkID,benchmarker.getBenchmark(benchmarkID));           
+                benchmarker.end(benchmarkID);
 
-            }catch(FileNotFoundException fnf){
-                System.out.printf("File %s not found\n",fileName);
+                // This median solution simply queries the list once or twice, as necessary
+                benchmarkID = "medNaive";
+                benchmarker.start(benchmarkID);
+                int medN;
+                if (list.length % 2 == 0) {
+                    medN = (list.get(list.length / 2 - 1) + list.get(list.length / 2)) / 2;
+                } else {
+                    medN = list.get(list.length / 2);
+                }
+                benchmarker.end(benchmarkID);
+
+                // This max solution simply iterates to the end of the list and then access it
+                // (I iterated manually because I optimized last-element access in the get
+                // method)
+                benchmarkID = "maxNaive";
+                benchmarker.start(benchmarkID);
+                iter = list.iterator();
+                while (iter.hasNext())
+                    iter.next();
+                int maxN = iter.currentNode.data;
+                benchmarker.end(benchmarkID);
+
+                // Using the iterator, this insert solution can use the node structure directly, doing it in at most 2/3 the time
+                list.clear();
+                fileScanner = new Scanner(new File(fileName));
+                benchmarkID = "insertImproved";
+                benchmarker.start(benchmarkID);
+                while (fileScanner.hasNextInt()) {
+                    int toInsert = fileScanner.nextInt();
+                    iter = list.iterator();
+                    while (iter.hasNext() && iter.currentNode.nextNode.data <toInsert) {
+                       iter.next();
+                    }
+                    iter.insertAfter(toInsert);
+                    if(list.length%50000==0){
+                        System.out.printf("%d items\n",list.length);
+                    }
+                }
+                benchmarker.end(benchmarkID);
+
+                // This better median solution manually iterates to the mediean index, allowing
+                // for quick next access, roughly halving the time
+                // The iterator stops 1 index short of the typical median index to ensure the
+                // ability to access the value before it as well
+                benchmarkID = "medImproved";
+                benchmarker.start(benchmarkID);
+                int medI;
+                int medIndex = list.length / 2-1;
+                LinkedList<Integer>.LinkedListNode curr = list.head.nextNode;
+                for (int i = 0; i < medIndex && curr.nextNode!=null; curr=curr.nextNode, i++) {
+                }
+                if (list.length % 2 == 0) {
+                    medI = (curr.data + curr.nextNode.data) / 2;
+                } else {
+                    medI = curr.data;
+                }
+                benchmarker.end(benchmarkID);
+
+                // Once again, the implementation allows max search to operate in O(1)
+                benchmarkID = "maxImproved";
+                benchmarker.start(benchmarkID);
+                int maxI = list.last.data;
+                benchmarker.end(benchmarkID);
+
+                // Thanks to the implementation of my LinkedList class, this min search is O(1) for both
+                benchmarkID = "min";
+                benchmarker.start(benchmarkID);
+                int min = list.head.nextNode.data;
+                benchmarker.end(benchmarkID);
+
+                System.out.printf(
+                        "\n-----------%s Bench Summary----------\n" + 
+                        "Min: %d\n" + "Med: %d\n" + "Max: %d\n"+ 
+                        "\n----------------------Time(Naive Attempt)---------------------\n" + 
+                        "Insert: %13dns||Min: %13dns||Med: %13dns||Max: %13dns\n" + 
+                        "\n----------------------Time(Improved Attempt)---------------------\n" + 
+                        "Insert: %13dns||Min: %13dns||Med: %13dns||Max: %13dns\n",
+                        fileName, min, medI, maxI, 
+                        benchmarker.getBenchmark("insertNaive"), 
+                        benchmarker.getBenchmark("min"),
+                        benchmarker.getBenchmark("medNaive"),
+                        benchmarker.getBenchmark("maxNaive"), 
+                        benchmarker.getBenchmark("insertImproved"), 
+                        benchmarker.getBenchmark("min"),
+                        benchmarker.getBenchmark("medImproved"),
+                        benchmarker.getBenchmark("maxImproved"));
+
+            } catch (FileNotFoundException e) {
+                System.out.printf("File %s not found\n", e);
             }
         }
     }
+    public static String[] getFiles(){
+        Scanner in = new Scanner(System.in);
+        LinkedList<String> paths = new LinkedList<>();
+        String currentPath;
+        while(true){
+            System.out.printf("\033[s");
+            System.out.printf("\033[uPlease enter a file name%s('/' is used as a path delimiter): ",paths.length==0?"":", or type RUN to begin benchmarking");
+            //https://stackoverflow.com/questions/2733255/java-doesnt-work-with-regex-s-says-invalid-escape-sequence
+            //Referenced to clear up minor regex confusion
+            while(!(currentPath = in.nextLine()).matches("(([^\n\t\r/]+/)*[^\n\t\r/]+)|RUN")){
+                System.out.printf("\033[uThat was an invalid path. Please enter a valid file name%s('/' is used as a path delimiter): ",paths.length==0?"":", or type RUN to begin benchmarking");
+            }
+            if(currentPath.equals("RUN")){
+                break;
+            }
+            paths.insert(currentPath);
+
+        }
+        String[] pathsArr = new String[paths.length];
+        LinkedList<String>.LinkedListIterator iter = paths.iterator();
+        for(int i =0; iter.hasNext();i++){
+            pathsArr[i] = iter.next();
+        }
+        in.close();
+        return pathsArr;
+    }
+
 }
--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,9 @@
 import java.util.Iterator;
-public class LinkedList<E> implements Iterable<E>{
+
+//References used to implement range for-each loop
+//https://docs.oracle.com/javase/8/docs/api/java/lang/Iterable.html
+//https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+public class LinkedList<E> implements Iterable<E> {
     public class LinkedListNode {
         E data;
         LinkedListNode nextNode;
@@ -8,17 +12,33 @@ public class LinkedList<E> implements Iterable<E>{
             data = e;
         }
     }
-    public class LinkedListIterator implements Iterator<E>{
-        LinkedListIterator(LinkedListNode node){
+
+    public class LinkedListIterator implements Iterator<E> {
+        LinkedListIterator(LinkedListNode node) {
             currentNode = node;
         }
+
         LinkedListNode currentNode;
-        public boolean hasNext(){
-            return currentNode.nextNode!=null;
+
+        public boolean hasNext() {
+            return currentNode.nextNode != null;
         }
-        public E next(){
+
+        public E next() {
             currentNode = currentNode.nextNode;
+            if (currentNode == null)
+                return null;
             return currentNode.data;
+        }
+
+        public void insertAfter(E e) {
+            LinkedListNode next = currentNode.nextNode;
+            currentNode.nextNode = new LinkedListNode(e);
+            currentNode.nextNode.nextNode = next;
+            if (next == null) {
+                last = currentNode.nextNode;
+            }
+            length++;
         }
     }
 
@@ -62,6 +82,10 @@ public class LinkedList<E> implements Iterable<E>{
     public void insert(E element, int index) {
         if (index > length)
             return;
+        else if (index == length) {
+            insert(element);
+            return;
+        }
         LinkedListNode previousNode = head;
         for (int i = 0; i < index; i++) {
             if (previousNode == null)
@@ -71,12 +95,17 @@ public class LinkedList<E> implements Iterable<E>{
         LinkedListNode oldNextNode = previousNode.nextNode;
         previousNode.nextNode = new LinkedListNode(element);
         previousNode.nextNode.nextNode = oldNextNode;
+        if (oldNextNode == null) {
+            last = previousNode.nextNode;
+        }
         length++;
     }
 
     public E get(int index) {
-        if (index >= length)
+        if (index > length)
             return null;
+        else if (index == length)
+            return last.data;
         LinkedListNode currentNode = head.nextNode;
         for (int i = 0; i < index; i++) {
             if (currentNode == null)
@@ -97,12 +126,23 @@ public class LinkedList<E> implements Iterable<E>{
             previousNode = previousNode.nextNode;
         }
         E data = previousNode.nextNode.data;
-        previousNode.nextNode = previousNode.nextNode.nextNode;
 
+        previousNode.nextNode = previousNode.nextNode.nextNode;
+        if (previousNode.nextNode == null) {
+            last = previousNode;
+        }
+        length--;
         return data;
     }
 
-    public LinkedListIterator iterator(){
+    public LinkedListIterator iterator() {
         return new LinkedListIterator(head);
     }
+
+    public void clear() {
+        head = new LinkedListNode(null);
+        last = head;
+        length = 0;
+    }
+
 }
