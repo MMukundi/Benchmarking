a40121a2007755d08caaad98baaa942141288f76
MMukundi <musicman5978@gmail.com>
2020-09-17 20:05:26 -0700

    Implement range for-each loop, insertion at index, and insert values from files into list

--- Benchmarking.java
+++ Benchmarking.java
@@ -1,5 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+//https://en.wikipedia.org/wiki/ANSI_escape_code
+//ANSI escape codes were used here for debugging and printing progress
+
 class Benchmarking{
     public static void main(String[] args) {
-        
+        for(String fileName:args){
+            try{
+            Scanner fileScanner = new Scanner(new File(fileName));
+            LinkedList<Integer> list = new LinkedList<Integer>();
+            System.out.printf("Inserting the values from %s\n\033[s",fileName);
+            while(fileScanner.hasNextInt()){
+                int toInsert = fileScanner.nextInt();
+                int insertionIndex=0;
+                for(int otherValue: list){
+                    if(otherValue<toInsert)break;
+                    insertionIndex++;
+                }
+                list.insert(toInsert,insertionIndex);
+                System.out.printf("\033[u\033[s%d values inserted",list.length);
+            }            
+
+            }catch(FileNotFoundException fnf){
+                System.out.printf("File %s not found\n",fileName);
+            }
+        }
     }
 }
--- LinkedList.java
+++ LinkedList.java
@@ -1,49 +1,108 @@
-public class LinkedList<E> {
-    class LinkedListNode{
+import java.util.Iterator;
+public class LinkedList<E> implements Iterable<E>{
+    public class LinkedListNode {
         E data;
         LinkedListNode nextNode;
-        LinkedListNode(E d){data=d;}
-    }
-    LinkedListNode head= new LinkedListNode(null);
-    LinkedListNode last;
-    int length=0;
-    LinkedList(E[] elems){
-        LinkedListNode previousNode = head;
-        for(E elem: elems){
-            LinkedListNode newNode = new LinkedListNode(elem);
-            previousNode.nextNode = newNode;
-            previousNode=newNode;
-            length++;
+
+        LinkedListNode(E e) {
+            data = e;
         }
-        last=previousNode;
+    }
+    public class LinkedListIterator implements Iterator<E>{
+        LinkedListIterator(LinkedListNode node){
+            currentNode = node;
+        }
+        LinkedListNode currentNode;
+        public boolean hasNext(){
+            return currentNode.nextNode!=null;
+        }
+        public E next(){
+            currentNode = currentNode.nextNode;
+            return currentNode.data;
+        }
     }
 
-    public void insert(E element){
+    LinkedListNode head = new LinkedListNode(null);
+    LinkedListNode last = head;
+    int length = 0;
+
+    LinkedList(E[] elems) {
+        LinkedListNode previousNode = head;
+        for (E elem : elems) {
+            LinkedListNode newNode = new LinkedListNode(elem);
+            previousNode.nextNode = newNode;
+            previousNode = newNode;
+            length++;
+        }
+        last = previousNode;
+    }
+
+    LinkedList(int length) {
+        LinkedListNode previousNode = head;
+        for (int i = 0; i < length; i++) {
+            LinkedListNode newNode = new LinkedListNode(null);
+            previousNode.nextNode = newNode;
+            previousNode = newNode;
+        }
+        last = previousNode;
+        this.length = length;
+    }
+
+    LinkedList() {
+
+    }
+
+    public void insert(E element) {
         LinkedListNode newNode = new LinkedListNode(element);
         last.nextNode = newNode;
-        last=newNode; 
-        length++;       
-    }  
-    public E get(int index){
-        if(index>=length)return null;
+        last = newNode;
+        length++;
+    }
+
+    public void insert(E element, int index) {
+        if (index > length)
+            return;
+        LinkedListNode previousNode = head;
+        for (int i = 0; i < index; i++) {
+            if (previousNode == null)
+                return;
+            previousNode = previousNode.nextNode;
+        }
+        LinkedListNode oldNextNode = previousNode.nextNode;
+        previousNode.nextNode = new LinkedListNode(element);
+        previousNode.nextNode.nextNode = oldNextNode;
+        length++;
+    }
+
+    public E get(int index) {
+        if (index >= length)
+            return null;
         LinkedListNode currentNode = head.nextNode;
-        for(int i=0; i < length;i++){
-            if(currentNode==null)return null;
+        for (int i = 0; i < index; i++) {
+            if (currentNode == null)
+                return null;
             currentNode = currentNode.nextNode;
         }
         return currentNode.data;
 
     }
-    public E remove(int index){
-        if(index>=length)return null;
+
+    public E remove(int index) {
+        if (index >= length)
+            return null;
         LinkedListNode previousNode = head;
-        for(int i=0; i < length;i++){
-            if(previousNode==null)return null;
+        for (int i = 0; i < index; i++) {
+            if (previousNode == null)
+                return null;
             previousNode = previousNode.nextNode;
         }
         E data = previousNode.nextNode.data;
         previousNode.nextNode = previousNode.nextNode.nextNode;
-        
+
         return data;
+    }
+
+    public LinkedListIterator iterator(){
+        return new LinkedListIterator(head);
     }
 }
