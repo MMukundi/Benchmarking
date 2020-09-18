ff4c891c281a6fa4e0b901120a8373665d80dc54
MMukundi <musicman5978@gmail.com>
2020-09-17 20:58:11 -0700

    Implemented benchmarking for crude insert

--- Benchmarker.java
+++ Benchmarker.java
@@ -1,61 +1,132 @@
 import java.util.Map;
+import java.io.StringWriter;
 import java.util.HashMap;
+import java.util.Random;
+import java.util.Stack;
 
 public class Benchmarker {
-    Map<String, Benchmark> benchmarks= new HashMap<String, Benchmark>();
-    String lastStarted;
-    Benchmarker(){}
-    public void start(String benchmarkName){
-        if(benchmarks.containsKey(benchmarkName)){
-            System.err.printf("Benchmark %s is already running\n",benchmarkName);
+    // Random Character Generator
+    static Random RCG = new Random();
+    Map<String, Benchmark> benchmarks = new HashMap<String, Benchmark>();
+    Stack<String> lastStarted = new Stack<String>();
+
+    Benchmarker() {
+    }
+
+    public void start(String benchmarkName) {
+        if (benchmarks.containsKey(benchmarkName)) {
+            System.err.printf("Benchmark %s is already running\n", benchmarkName);
             return;
         }
         Benchmark newBenchmark = new Benchmark();
         benchmarks.put(benchmarkName, newBenchmark);
 
-
-        // Start time recorded at the very end of the function to ensure execution as near as possible to next line
+        // Start time recorded at the very end of the function to ensure execution as
+        // near as possible to next line
+        lastStarted.push(benchmarkName);
         newBenchmark.start = System.nanoTime();
     }
-    public void end(String benchmarkName){
-        // End time recorded at the very beginning of the function to ensure execution as near as possible to previous line
+
+    public void end(String benchmarkName) {
+        // End time recorded at the very beginning of the function to ensure execution
+        // as near as possible to previous line
         long endTime = System.nanoTime();
-        if(!benchmarks.containsKey(benchmarkName)){
-            System.err.printf("Benchmark %s has not started\n",benchmarkName);
+        if (!benchmarks.containsKey(benchmarkName)) {
+            System.err.printf("Benchmark %s has not started\n", benchmarkName);
             return;
         }
-
         Benchmark toEnd = benchmarks.get(benchmarkName);
-        if(toEnd.ended){
-            System.err.printf("Benchmark %s has already ended\n",benchmarkName);
+        if (toEnd.ended) {
+            System.err.printf("Benchmark %s has already ended\n", benchmarkName);
             return;
         }
+        lastStarted.remove(benchmarkName);
 
-       toEnd.end=endTime;
-       toEnd.ended=true;
+        toEnd.end = endTime;
+        toEnd.ended = true;
     }
-    public long getBenchmark(String benchmarkName){
-        if(!benchmarks.containsKey(benchmarkName)){
-            System.err.printf("Benchmark %s has not started\n",benchmarkName);
+
+    public long getBenchmark(String benchmarkName) {
+        if (!benchmarks.containsKey(benchmarkName)) {
+            System.err.printf("Benchmark %s has not started\n", benchmarkName);
             return -1;
         }
         Benchmark benchmark = benchmarks.get(benchmarkName);
-        return benchmark.end-benchmark.start;
+        if (!benchmark.ended) {
+            System.err.printf("Benchmark %s is still running\n", benchmarkName);
+            return -1;
+        }
+        return benchmark.end - benchmark.start;
     }
+
+    public String start() {
+        // Generates a pseudo random newString
+        StringWriter newString = new StringWriter();
+        for (int i = 0; i < 10; i++) {
+            newString.append((char) ('a' + RCG.nextInt(26)));
+        }
+        String s = newString.toString();
+
+        start(s);
+        return s;
+    }
+
+    public void end() {
+        // End time recorded at the very beginning of the function to ensure execution
+        // as near as possible to previous line
+        long endTime = System.nanoTime();
+        if(lastStarted.empty()){
+            System.err.printf("No active benchmarks\n");
+            return;
+        }
+        String toEndName = lastStarted.pop();
+        if (!benchmarks.containsKey(toEndName)) {
+            System.err.printf("Benchmark %s has not started\n", toEndName);
+            return;
+        }
+
+        Benchmark toEnd = benchmarks.get(toEndName);
+        if (toEnd.ended) {
+            System.err.printf("Benchmark %s has already ended\n", toEndName);
+            return;
+        }
+
+        toEnd.end = endTime;
+        toEnd.ended = true;
+    }
+
+    //While possibly useful, I found would involve a "last ended" stack
+
+    // public long getBenchmark() {
+    //     String lastEndedName = lastEnded.peek();
+    //     if (!benchmarks.containsKey(lastEndedName)) {
+    //         System.err.printf("Benchmark %s has not started\n", lastEndedName);
+    //         return -1;
+    //     }
+    //     Benchmark benchmark = benchmarks.get(lastEndedName);
+    //     if (!benchmark.ended) {
+    //         System.err.printf("Benchmark %s is still running\n", lastEndedName);
+    //         return -1;
+    //     }
+    //     return benchmark.end - benchmark.start;
+    // }
 }
 
-class Benchmark{
-    long start=Long.MIN_VALUE;
-    long end=Long.MIN_VALUE;
-    boolean ended=false;
-    Benchmark(long start,long end){
-        this.start=start;
-        this.end=end;
+class Benchmark {
+    long start = Long.MIN_VALUE;
+    long end = Long.MIN_VALUE;
+    boolean ended = false;
+
+    Benchmark(long start, long end) {
+        this.start = start;
+        this.end = end;
     };
-    Benchmark(long start){
-        this.start=start;
+
+    Benchmark(long start) {
+        this.start = start;
     };
-    Benchmark(){
+
+    Benchmark() {
 
     };
     // System
--- Benchmarking.java
+++ Benchmarking.java
@@ -7,10 +7,14 @@ import java.util.Scanner;
 
 class Benchmarking{
     public static void main(String[] args) {
+        Benchmarker benchmarker= new Benchmarker();
         for(String fileName:args){
             try{
             Scanner fileScanner = new Scanner(new File(fileName));
             LinkedList<Integer> list = new LinkedList<Integer>();
+
+            String benchmarkID = fileName+"_insert";
+            benchmarker.start(benchmarkID);
             System.out.printf("Inserting the values from %s\n\033[s",fileName);
             while(fileScanner.hasNextInt()){
                 int toInsert = fileScanner.nextInt();
@@ -22,7 +26,8 @@ class Benchmarking{
                 list.insert(toInsert,insertionIndex);
                 System.out.printf("\033[u\033[s%d values inserted",list.length);
             } 
-            System.out.println("")           
+            benchmarker.end(benchmarkID);
+            System.out.printf("\n%s took %d ns\n",benchmarkID,benchmarker.getBenchmark(benchmarkID));           
 
             }catch(FileNotFoundException fnf){
                 System.out.printf("File %s not found\n",fileName);
